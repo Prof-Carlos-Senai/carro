a3f2da19f5c2f142c2524c4c33d4b36dca982105
Roberto dev1 <carlos.dev.jlle@gmail.com>
2024-02-23 06:08:55 -0300

    Criada as classes Obj, Carro e Texto

---
+++ f1_class.js
@@ -0,0 +1,39 @@
+class Obj{
+    constructor(x,y,w,h,a){
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.a = a
+    }
+}
+
+class Carro extends Obj{
+
+    dir = 0
+    pts = 0
+    vida = 0
+
+    des_car(){
+
+    }
+
+    mov_car(){
+
+    }
+
+    point(){   // boolean
+
+    }
+
+    colid(){   // boolean
+
+    }
+
+}
+
+class Texto{
+    des_text(){
+
+    }
+}
